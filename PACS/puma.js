101c2441345829b916ab0eec0e355a9f22d480b5
henderson2k <henderson2k@users.noreply.github.com>
2024-06-27 15:09:16 +0100

    Update PACS/puma.js

--- PACS/puma.js
+++ PACS/puma.js
@@ -10,7 +10,6 @@ setTimeout(function(){
     });
 
 var clearform = document.getElementById('ctl00_ContentPlaceHolder1_btnClear');
-ctl00_ContentPlaceHolder1_btnClear
 if (clearform) {
     // Trigger a click event on the button
     clearform.click();
