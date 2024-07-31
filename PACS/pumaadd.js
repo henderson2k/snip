21e20d06438431aec3e98806176e8d8e6df31437
henderson2k <henderson2k@users.noreply.github.com>
2024-07-31 11:59:46 +0100

    Update PACS/pumaadd.js

--- PACS/pumaadd.js
+++ PACS/pumaadd.js
@@ -5,7 +5,7 @@ setTimeout(function() {
        // console.log('Text from clipboard:', text);
   
         // Split the clipboard text by commas into an array  (Firstname, Lastname, Speciality/Department, Group, eMail, GMC/Registration, Description, UserName)
-  
+  // Fir
   
         var valuesArray = text.split(',').map(value => value.trim());
   var DescriptionArray = valuesArray[6].split('_').map(value => value.trim());
