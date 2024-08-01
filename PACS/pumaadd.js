e78c58cdb5dd5c9a2c1625535cc80435d683084f
henderson2k <henderson2k@users.noreply.github.com>
2024-08-01 22:08:53 +0100

    Update PACS/pumaadd.js

--- PACS/pumaadd.js
+++ PACS/pumaadd.js
@@ -18,7 +18,7 @@ setTimeout(function() {
   document.getElementById('ctl00_ContentPlaceHolder1_txtFName').value = valuesArray[0];
   document.getElementById('ctl00_ContentPlaceHolder1_txtSName').value = valuesArray[1];
   document.getElementById('ctl00_ContentPlaceHolder1_txtProfReg').value = valuesArray[3];
-  document.getElementById('ctl00_ContentPlaceHolder1_txtEmailAddress').value = valuesArray[4];
+  document.getElementById('ctl00_ContentPlaceHolder1_txtEmailAddress').value = valuesArray[3];
 
   
   // Set Speciality
