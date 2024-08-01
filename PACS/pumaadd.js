cdd28b2d4f0fe4eda19cb185e3a5e6e4d48a57db
henderson2k <henderson2k@users.noreply.github.com>
2024-08-01 16:13:02 +0100

    Update PACS/pumaadd.js

--- PACS/pumaadd.js
+++ PACS/pumaadd.js
@@ -4,9 +4,6 @@ setTimeout(function() {
       .then(text => {
        // console.log('Text from clipboard:', text);
   
-//                    0          1       2     3     4             5            6         7           8      9
- //// Clipboard:  Firstname, Lastname, Grade, GMC, eMail, MothersMaidenName, Username, Description, Group, Spec
-
 
 //                 0          1       2     3     4           5             6        7         8          9
 // Clipboard:  Firstname, Lastname, Grade, GMC, eMail, MothersMaidenName, Group, Username, Description,  Spec
