4c8d7050d86e011901fc6f1b21bc0b262b1722a8
henderson2k <henderson2k@users.noreply.github.com>
2024-08-01 15:01:09 +0100

    Update PACS/pumaadd.js

--- PACS/pumaadd.js
+++ PACS/pumaadd.js
@@ -4,7 +4,7 @@ setTimeout(function() {
       .then(text => {
        // console.log('Text from clipboard:', text);
   
-//                    0          1         
+//                    0          1       
  //// Clipboard:  Firstname, Lastname, Grade, GMC, eMail, MothersMaidenName, Username, Description, Group, Spec
 
         var valuesArray = text.split(',').map(value => value.trim());
